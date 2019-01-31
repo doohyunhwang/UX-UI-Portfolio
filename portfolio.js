881788c134a1d02c4b228545956e9eb8447fd939
Unknown <doohyunhwang1@gmail.com>
2019-01-30 18:30:12 -0800

    case study

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+(function scrollReveal() {
+    window.sr = ScrollReveal();
+    
+    sr.reveal('.card, .enabling-img, .airbnb-img, .travel-img', {
+      duration   : 600,
+      distance   : '20px',
+      easing     : 'ease-out',
+      origin     : 'bottom',
+      reset      : true,
+      scale      : 1,
+      viewFactor : 0,
+      afterReveal  : revealChildren,
+    }, 150);
+    
+      var revealChildren = sr.reveal('.card-title, .card-text', {
+      duration   : 500,
+      scale      : 1,
+      distance   : '20px',
+      origin     : 'bottom',
+      reset      : true,
+      easing     : 'ease-out',
+      viewFactor : 1,
+    }, 75);
+  })();
+  
